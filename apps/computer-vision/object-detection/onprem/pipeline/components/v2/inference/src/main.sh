38b7f253feceabfbff20c221ca8c66a14d2bc1e5
PoornimaDevii <poornimadevi.rama@gmail.com>
2020-10-29 07:00:57 -0700

    modify  inference comp

--- apps/computer-vision/object-detection/onprem/pipeline/components/v2/inference/src/main.sh
+++ apps/computer-vision/object-detection/onprem/pipeline/components/v2/inference/src/main.sh
@@ -4,19 +4,6 @@ set -x
 
 cd /opt/yolov3-tf2/
 
-python3 tools/voc2012.py \
-  --data_dir '/mnt/tfjob/VOCdevkit/VOC2012/' \
-  --split train \
-  --output_file ./data/voc2012_train.tfrecord
+python3 detect.py
 
-python3  tools/voc2012.py \
-  --data_dir '/mnt/tfjob/VOCdevkit/VOC2012/' \
-  --split val \
-  --output_file ./data/voc2012_val.tfrecord
-
-python3 convert.py
-python3 train.py
-
-cp -r trained_model/ /mnt/tfjob
-cp -r checkpoints_keras/ /mnt/tfjob
 
